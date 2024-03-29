ef754b2af992e91e445b1b3402c3709199d59ace
Livio Tinnirello <tinnirellolivio@gmail.com>
2024-03-30 00:36:56 +0100

    feat(repository): Refactoring function verifyUserPassword

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -1,9 +1,13 @@
-import type {VerifyUserPasswordRequest} from "@/repository/types"
+import {
+  TwoFactorAuthenticatorTypeRequest,
+  TwoFactorAuthenticatorTypeResponse,
+  VerifyPasswordRequest
+} from "@/http/rest/types"
 import {getErrorCodeFromProblem} from "@/http/http-problem"
 import {HttpErrorCode} from "@/http/http-error-code"
 
-export async function verifyUserPassword(req: VerifyUserPasswordRequest) {
-  const res = await fetch("/api/login/verify/password", {
+export async function verifyUserPassword(req: VerifyPasswordRequest) {
+  const res = await fetch("/api/auth/verify/password", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
@@ -11,10 +15,34 @@ export async function verifyUserPassword(req: VerifyUserPasswordRequest) {
     body: JSON.stringify(req)
   })
 
-  console.log(res.status)
-
   if (res.status === 200) {
     return "password_valid"
+  } else {
+    const body = await res.json()
+    const errorCode = getErrorCodeFromProblem(body)
+    switch (errorCode) {
+      case HttpErrorCode.EMAIL_NOT_FOUNT:
+        return "email_not_found"
+      case HttpErrorCode.PASSWORD_IS_INVALID:
+        return "password_invalid"
+    }
+
+    throw new Error(`Undefined error code from server ${errorCode}`)
+  }
+}
+
+export async function twoFactorAuthenticatorUserType(req: TwoFactorAuthenticatorTypeRequest) {
+  const res = await fetch("/api/auth/two-factor", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(req)
+  })
+
+  if (res.status === 200) {
+    const body = await res.json()
+    return TwoFactorAuthenticatorTypeResponse.parse(body)
   } else {
     const body = await res.json()
     const errorCode = getErrorCodeFromProblem(body)
