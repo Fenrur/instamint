388f6d8bbdd937dcef8fbcd901b5a13bfd557aba
Livio Tinnirello <tinnirellolivio@gmail.com>
2024-03-30 00:23:37 +0100

    feat: Edit schema for the db email begin unique

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,7 +3,7 @@ import {boolean, pgTable, serial, timestamp, uuid, varchar} from "drizzle-orm/pg
 export const user = pgTable("User", {
   id: serial("id").primaryKey(),
   uid: uuid("uid").unique().notNull(),
-  email: varchar("email", { length: 256 }).notNull(),
+  email: varchar("email", { length: 256 }).unique().notNull(),
   hashedPassword: varchar("hashedPassword", { length: 256 }).notNull(),
   twoFactorEnabled: boolean("twoFactorEnabled").notNull().default(false),
   twoFactorSecret: varchar("twoFactorSecret", { length: 256 }),
