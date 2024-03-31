2617de5582a2363fbc023744482ddea14f55a47e
Livio Tinnirello <tinnirellolivio@gmail.com>
2024-03-31 18:03:33 +0200

    feat: Change page login

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,8 +1,9 @@
 "use server"
 import React from "react"
+import {useSearchParams} from "next/navigation"
 
 export default async function LayoutLoginPage({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode
 }>) {
@@ -15,7 +16,7 @@ export default async function LayoutLoginPage({
           <div className="grid gap-2 text-center">
             <h1 className="text-3xl font-bold">Login</h1>
             <p className="text-balance text-muted-foreground">
-              Enter your email below to login to your account
+              Fill below to login to your account
             </p>
           </div>
           {children}
