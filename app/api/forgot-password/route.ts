c5a87d1e1b4ea365686961f485f4eea3f0ce81e3
ouafaema <marhrouf.ouafae@gmail.com>
2024-05-04 21:54:59 +0000

    feat: reset password

--- app/api/forgot-password/route.ts
+++ app/api/forgot-password/route.ts
@@ -12,22 +12,24 @@ import {emailVerificationService, userService} from "@/services"
 
 async function createVerificationAndSendEmail(formData: SignupCredentials, createdAt: DateTime<true>) {
   const verificationId = await emailVerificationService.create(formData.email, createdAt)
+
   const verificationLink = u.format({
     host: env.BASE_URL,
-    pathname: "/api/signup/verification-email",
+    pathname: "/api/forgot-password/verification-email",
     query: {vid: verificationId},
   })
+
   const emailHtml = render(VerificationEmail({
     baseUrl: env.BASE_URL,
     contactEmail: env.CONTACT_EMAIL,
     instamintImageUrl: `${env.BASE_URL}/instamint.svg`,
     verificationLink,
-    bodyMassage: "Verify your email Instamint",
+    bodyMassage: "Reset your password Instamint",
   }))
 
   await transporter.sendMail({
     to: formData.email,
-    subject: "Verify email Instamint",
+    subject: "Reset your password Instamint",
     html: emailHtml,
   })
 }
@@ -37,39 +39,30 @@ export async function POST(req: NextRequest) {
     return problem({...invalidContentTypeProblem, detail: "Content-Type must be application/x-www-form-urlencoded"})
   }
 
-  const createdAt = DateTime.now()
-  const formData = await req.formData()
-  let parsedFormData = null
-  const url = req.nextUrl.clone()
+  const createdAt = DateTime.now();
+  const formData = await req.formData();
+  let parsedFormData = null;
+  const url = req.nextUrl.clone();
 
   try {
-    parsedFormData = SignupCredentials.parse(formData)
+    parsedFormData = SignupCredentials.parse(formData);
   } catch (e: any) {
-    url.pathname = "/signup"
-
-    return NextResponse.redirect(url)
+    url.pathname = "/signup";
+    return NextResponse.redirect(url);
   }
 
-  const emailDb = await userService.findByEmail(parsedFormData.email)
+  const user = await userService.findByEmail(parsedFormData.email);
 
-  if (emailDb) {
-    url.pathname = "/signup"
-    url.searchParams.set("error", "email_exists")
-
-    return NextResponse.redirect(url)
+  if (!user) {
+    url.pathname = "/forgot-password";
+    url.searchParams.set("error", "email_not_exists");
+    return NextResponse.redirect(url);
   }
 
   const emailVerifications = await emailVerificationService.findUnverifiedAndBeforeExpirationByEmail(
     parsedFormData.email,
     createdAt
   )
-
-  if (emailVerifications && emailVerifications.length >= 5) {
-    url.pathname = "/signup"
-    url.searchParams.set("error", "email_verification_limit_exceeded")
-
-    return NextResponse.redirect(url)
-  }
 
   url.pathname = "/verification-email/sent"
   url.searchParams.set("email", parsedFormData.email)
--- src/mail/templates/verification-email.tsx
+++ src/mail/templates/verification-email.tsx
@@ -20,6 +20,7 @@ interface VerificationEmailProps {
   instamintImageUrl: string;
   verificationLink: string;
   contactEmail: string;
+  bodyMassage: string;
 }
 
 export const VerificationEmail = ({
@@ -27,6 +28,7 @@ export const VerificationEmail = ({
                                     instamintImageUrl,
                                     verificationLink,
                                     contactEmail,
+                                    bodyMassage
                                   }: VerificationEmailProps) => {
   const previewText = `Verify your mail on Instamint`
 
@@ -46,7 +48,7 @@ export const VerificationEmail = ({
               />
             </Section>
             <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
-              <strong>Verify your email on <Link href={baseUrl}
+              <strong>{bodyMassage} <Link href={baseUrl}
                                                  className="text-green-400 underline">Instamint</Link></strong>
             </Heading>
             <Section className="text-center mt-[32px] mb-[32px]">
@@ -54,7 +56,7 @@ export const VerificationEmail = ({
                 className="bg-[#000000] rounded text-white text-[12px] font-semibold no-underline text-center px-5 py-3"
                 href={verificationLink}
               >
-                Verify Email
+                  Verify
               </Button>
             </Section>
             <Text className="text-black text-[14px] leading-[24px]">
