4ee81a3805e72d8b9976235b8424ee15b9c7f541
ouafaema <marhrouf.ouafae@gmail.com>
2024-05-09 21:14:16 +0000

    chore: split email templates

--- src/mail/templates/verification-email.tsx
+++ src/mail/templates/verification-email.tsx
@@ -20,7 +20,6 @@ interface VerificationEmailProps {
   instamintImageUrl: string;
   verificationLink: string;
   contactEmail: string;
-  bodyMassage: string;
 }
 
 export const VerificationEmail = ({
@@ -28,7 +27,6 @@ export const VerificationEmail = ({
                                     instamintImageUrl,
                                     verificationLink,
                                     contactEmail,
-                                    bodyMassage
                                   }: VerificationEmailProps) => {
   const previewText = `Verify your mail on Instamint`
 
@@ -48,7 +46,7 @@ export const VerificationEmail = ({
               />
             </Section>
             <Heading className="text-black text-[24px] font-normal text-center p-0 my-[30px] mx-0">
-              <strong>{bodyMassage} <Link href={baseUrl}
+              <strong>Verify your email Instamint <Link href={baseUrl}
                                                  className="text-green-400 underline">Instamint</Link></strong>
             </Heading>
             <Section className="text-center mt-[32px] mb-[32px]">
